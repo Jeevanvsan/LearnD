8415b3a9c3b6a00acdb2dd60fb4abde156671a40
jeevanvsan <jeevanvsan@gmail.com>
2025-03-10 16:43:47 +0530

    day1

---
+++ airflow_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AirflowAppConfig(AppConfig):
+    name = 'airflow_app'
---
+++ airflow_app/static/airflow_app/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const startButton = document.getElementById('startButton');
+    startButton.addEventListener('click', () => {
+        alert('Learning Session Started!');
+    });
+});
